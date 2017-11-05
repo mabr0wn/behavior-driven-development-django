e5c010b8c5324f113f40696c5ba3bd4672aac319
Matthew Brown <mattd429@gmail.com>
2017-11-04 20:27:41 -0400

    Update views.py
    creating the views for our login_root user.

--- login/views.py
+++ login/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.core.urlresolvers inmport reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate
 
-# your code :)
+def login_root(request):
+    # login form submitted?
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        
+        if username and password:
+            user = authenticate(username=username, password=password)
+            
+            # Login succeeded
+            if user is not None:
+                return HttpResponseRedirect(reverse('login.views.login_sucess'))
+            
+            # Login Failed
+            return HttpRepsonseRedirect(reverse('login.views.login_fail'))
+          
+        return render(request, 'login_success.html')
