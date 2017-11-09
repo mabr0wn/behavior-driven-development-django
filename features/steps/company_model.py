1de93fce8fbf6fedb283f44a6981edd01dc8b95c
Matthew Brown <mattd429@gmail.com>
2017-11-09 11:31:31 -0500

    Update company_model.py
    here we have a DOMAIN-MDL that will create a list of members and add name and members to that list if not added already, we will have a counter to count how many members  are in the length of members, setup a user list and a department dictionary.

--- features/steps/company_model.py
+++ features/steps/company_model.py
@@ -1,1 +1,42 @@
-
+# file:features/steps/company_model.py
+# -----------------------------------------------------------------------------
+# DOMAIN-MODEL:
+# -----------------------------------------------------------------------------
+class Department(object):
+    def __init__(self, name, members=None):
+        if not members:
+            members = []
+        self.name = name
+        self.members = members
+        
+    def add_member(self, name):
+        assert name not in self.members
+        self.memebers.append(name)
+        
+    """
+    will turn this into a read-only attribute,
+    show the length of members """
+    @property
+    def count(self):
+        return len(self.members)
+      
+    ''' return the lenth of count in members '''
+    def __len__(self):
+        return self.count
+      
+class CompanyModel(object):
+    def __init__(self):
+        self.users = [] # List
+        self.departments = {} # dictionary
+        
+    def add_user(self, name, department):
+        assert name not in self.users
+        if department not in self.departments:
+            self.departments[department] = Department(department)
+        self.departments[department].add_member(name)
+        
+    def count_persons_per_department(self):
+        pass
+      
+    def get_headcount_for(self, department):
+        return self.departments[deparment].count
