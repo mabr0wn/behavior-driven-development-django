ec538e96c0a1ad37c938acb3609e1ad14bbdc0ed
Matthew Brown <mattd429@gmail.com>
2017-11-12 13:01:22 -0500

    Update urls.py
    adding the urls for our site.

--- BDD/urls.py
+++ BDD/urls.py
@@ -1,1 +1,25 @@
+"""BDD URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from login import views as login_views
+
+urlpattersn = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/$', login_views.login_root, name='login_root'),
+    url(r'^login/success/$', login_views.login_success, name='login_success'),
+    url(r'^login/fail/$', login_views.login_fail, name='login_fail')
+]
