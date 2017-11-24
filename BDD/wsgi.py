75df5f5372be090037fce7670514010103572b75
Matthew Brown <mattd429@gmail.com>
2017-11-24 18:28:43 -0500

    Update wsgi.py

--- BDD/wsgi.py
+++ BDD/wsgi.py
@@ -1,1 +1,9 @@
+"""
+WSGI config for bdd_example project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
 
